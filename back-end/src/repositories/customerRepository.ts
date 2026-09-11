ef985f965cd4b39e33454ba0b0ac541f9294c44f
Gabriel Bortoloti <gabrielbortoloti2016@gmail.com>
2026-09-11 00:48:49 +0000

    (10/09) Inserção do primeiro cliente no banco de dados (teste CRUD)

--- back-end/src/repositories/customerRepository.ts
+++ back-end/src/repositories/customerRepository.ts
@@ -1,50 +1,46 @@
 import { prisma } from "../database/client.ts";
 
-
 import type { CreateCustomerDto }
  from "../dto/customer/createCustomerDto.ts";
-
 
 import type { UpdateCustomerDto }
  from "../dto/customer/updateCustomerDto.ts";
 
+export function create(data: CreateCustomerDto) {
+  return prisma.customer.create({
+    data: {
+      ...data,
+      birth_date: data.birth_date ? new Date(data.birth_date) : null,
+    },
+  })
+}
 
 export function findAll() {
- return prisma.customer.findMany({
-   orderBy: {
-     name: "asc",
-   },
- });
+  return prisma.customer.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
 }
-
 
 export function findById(id: number) {
- return prisma.customer.findUnique({
-   where: { id },
- });
+  return prisma.customer.findUnique({
+    where: { id },
+  });
 }
-
-
-export function create(data: CreateCustomerDto) {
- return prisma.customer.create({
-   data,
- });
-}
-
 
 export function update(
- id: number,
- data: UpdateCustomerDto
+  id: number,
+  data: UpdateCustomerDto
 ) {
- return prisma.customer.update({
-   where: { id },
-   data,
- });
+  return prisma.customer.update({
+    where: { id },
+    data,
+  });
 }
 
-
 export function remove(id: number) {
- return prisma.customer.delete({
-   where: { id },
- });
+  return prisma.customer.delete({
+    where: { id },
+  });
 }
